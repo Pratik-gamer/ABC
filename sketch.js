4fe7323dc1bf12783ed3b4607e1cf799a705325e
Pratik-gamer <73925938+Pratik-gamer@users.noreply.github.com>
2020-12-02 20:34:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world, dustbinImage, paper, paperImage;
+
+function preload(){
+paperImage=loadImage("paper.png")
+
+}
+function setup() {
+  createCanvas(800, 400);
+  rectMode(CENTER);
+
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  dustbin = new DustBin(720, 390, 100, 10);
+  paper = new Paper(100, 300, 10);
+  paper.addImage(paperImage);
+  ground = Bodies.rectangle(width / 2, 400, width, 10,
+  {
+    isStatic: true
+  });
+  World.add(world, ground);
+}
+
+function draw() {
+     background("black");
+       rectMode(CENTER);
+    background(0);
+
+    dustbin.display();
+    paper.display();
+    if (keyDown("space")) {
+      Matter.Body.applyForce(paper.body, paper.body.position, {
+        x: 5,
+        y: -10
+      });
+    }
+  }
